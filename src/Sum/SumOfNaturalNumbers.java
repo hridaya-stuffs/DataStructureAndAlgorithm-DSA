334a839c98b5a4249c475b04ca3cd32579e88c0d
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-03-30 13:07:12 +0545

    Sum: Add example for sum of natural numbers
    Sum of natural numbers from 1 to 100

---
+++ src/Sum/SumOfNaturalNumbers.java
@@ -0,0 +1,12 @@
+package Sum;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        int n = 100;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of natural numbers from 1 to " + n + " is " + sum);
+    }
+}
