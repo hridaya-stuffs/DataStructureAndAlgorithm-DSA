a6084fa110e1983b323fab03e2833e6f8447cda3
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-03-30 13:02:54 +0545

    Array: Add ArraySorting

---
+++ src/Array/Sort/ArraySort.java
@@ -0,0 +1,20 @@
+package Array.Sort;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        int[] arr = {6, 4, 9, 6, 3, 2, 0};
+        System.out.print("Elements after sorting:");
+        for (int i = 0; i < arr.length; i++) {
+            int temp = 0;
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            System.out.print(" " + arr[i]);
+        }
+        System.out.println();
+    }
+}
